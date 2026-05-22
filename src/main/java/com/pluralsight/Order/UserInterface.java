aa7de356690f4c0569a765fead57193d1687c19e
Batdrew1233 <drewstevenson1233@gmail.com>
2026-05-22 15:08:25 -0400

    Finished template for orderScreen

--- src/main/java/com/pluralsight/Order/UserInterface.java
+++ src/main/java/com/pluralsight/Order/UserInterface.java
@@ -15,13 +15,14 @@ public class UserInterface {
         while (!quit){
             System.out.println("\n");
             System.out.println("====== Welcome to the shop =====");
-            System.out.println("1) New Order");
-            System.out.println("0) Exit");
+            System.out.println(" 1) New Order");
+            System.out.println(" 0) Exit");
             System.out.print("Your choice: ");
             String choice = scanner.nextLine();
 
             switch (choice){
                 case "1":
+                    order = new Order();
                     orderScreen();
                     break;
                 case "2":
@@ -40,7 +41,38 @@ public class UserInterface {
             System.out.println("\n");
             //Print cart here (Newest first)
             System.out.println("\n");
+            System.out.println("Order Menu");
+            System.out.println(" 1) Add Item");
+            System.out.println(" 2) Add Drink");
+            System.out.println(" 3) Add chips");
+            System.out.println(" 4) Checkout");
+            System.out.println(" 0) Cancel Order");
+            System.out.print("Your choice: ");
+            String choice = scanner.nextLine();
+
+            switch (choice){
+                case "1":
+                    //Go to make sandwich
+                    break;
+                case "2":
+                    //Go to make drink
+                    break;
+                case "3":
+                    //Go to add chips
+                    break;
+                case "4":
+                    //Go to check out
+                    break;
+                case "0":
+                    cancelOrder = true;
+                    break;
+                default:
+                    System.out.println("Invalid choice");
+            }
 
         }
     }
+
+
+
 }
