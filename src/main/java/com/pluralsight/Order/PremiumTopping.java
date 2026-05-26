879a051a6459094f462109f7b5d370c929b6d024
Batdrew1233 <drewstevenson1233@gmail.com>
2026-05-26 12:10:17 -0400

    Completed structure for Toppings and PremiumTopping class

---
+++ src/main/java/com/pluralsight/Order/PremiumTopping.java
@@ -0,0 +1,25 @@
+package com.pluralsight.Order;
+
+public abstract class PremiumTopping extends Topping {
+    private boolean extra;
+
+    public PremiumTopping(String name, boolean extra) {
+        super(name);
+        this.extra = extra;
+    }
+
+    public boolean isExtra() {
+        return extra;
+    }
+
+    public double getPrice(String size){
+
+    }
+
+    @Override
+    public String getName() {
+        return super.getName();
+    }
+
+
+}
---
+++ src/main/java/com/pluralsight/Order/Topping.java
@@ -0,0 +1,13 @@
+package com.pluralsight.Order;
+
+public abstract class Topping {
+    private String name;
+
+    public Topping(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
